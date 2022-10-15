0f4d026d74d4a88b7c4b1e4357eda737fde72229
rjames187 <rory.james2021@gmail.com>
2022-10-15 18:21:23 -0500

    implement words with most syllables

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -5,15 +5,18 @@ import '../accordion.js'
 import { countSyllables } from '../functions/syllable';
 
 function Calc() {
-
+  
+  const [input, setInput] = useState([])
   const [count, setCount] = useState({sentences: 0, words: 0, syllables: 0});
   const [score, setScore] = useState(0);
+  const [hardestWords, setHardestWords] = useState([]);
 
   const countText = (e) => {
     let text = e.target.value.trim();
 
     if (text === "") {
         setCount({sentences: 0, words: 0, syllables: 0});
+        setInput([]);
         return;
     }
 
@@ -25,7 +28,10 @@ function Calc() {
     let numWords = 0;
     let numSyllables = 0;
 
+    let tempInput = []
     sentences.forEach((sent) => {
+        let tempSent = []
+
         sent = sent.trim();
         const words = sent.split(" ");
         numWords += words.length;
@@ -33,16 +39,46 @@ function Calc() {
         // count syllables
         words.forEach((word) => {
             numSyllables += countSyllables(word);
+            tempSent.push(word);
         });
-    })
 
+        tempInput.push(tempSent);
+    })
+    setInput(tempInput);
     setCount({sentences: sentences.length, words: numWords, syllables: numSyllables});
   }
 
-  // calculate readability score
+  // calculations made when state updates
   useEffect(() => {
+    // calculate readability score
     setScore(206.835 - 1.015 * (count.words / count.sentences) - 84.6 * (count.syllables / count.words));
   }, [count]);
+
+  useEffect(() => {
+    // calculate most difficult words and sentences
+    let hardWords = new Map();
+    let hardSentences = new Map();
+
+    input.forEach((sent) => {
+        let sentSyllables = 0;
+        sent.forEach((word) => {
+            let wordSyllables = countSyllables(word);
+            hardWords.set(word, wordSyllables);
+            sentSyllables += wordSyllables;
+        });
+        hardSentences.set(sent, sentSyllables);
+    });
+
+    hardWords = new Map([...hardWords].sort((a, b) => b[1] - a[1]));
+
+    let hardWordsArray = []
+    hardWords.forEach((value, key) => {
+        if (value > 3 && hardWordsArray.length <= 10) {
+            hardWordsArray.push(key);
+        }
+    });
+    setHardestWords(hardWordsArray);
+  }, [input])
 
   return (
     <>
@@ -51,6 +87,10 @@ function Calc() {
         <p>Words: {count.words}</p>
         <p>Syllables: {count.syllables}</p>
         <p>Score: {parseFloat(score).toFixed(2)}</p>
+
+        <ul>
+            { hardestWords.map((word) => <li>{word}</li>) };
+        </ul>
     </>
   );
 }
