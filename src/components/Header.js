2427e805a5f08b4a2f6edd87cc622eda29ab2657
rjames187 <rory.james2021@gmail.com>
2022-10-16 04:24:54 -0500

    minor styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
       <header>
           <h1>Readability Calculator</h1>
       </header>
-      <h2><a href="#background">#Learn How This Works and Why It's Useful#</a></h2>
+      <h2><a href="#background">⬇️Learn How This Works⬇️</a></h2>
     </>
   );
 }
